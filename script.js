4178b0b63cf4fdf4eb4da28d59ba9349572b5e15
Jhon Gonzalez <66422237+jhongonzalezweb@users.noreply.github.com>
2022-05-23 11:37:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+
+let numeroCuadrados = 6;
+let colors = [];
+let colorsRandom = "";
+let pickedColor = colors[3];
+
+function iniciarElJuegoMomentaneamente() {
+
+    function asignarColoresTarjetas() {
+        var redColor = Math.floor(Math.random() * 255);
+        var greenColor = Math.floor(Math.random() * 255);
+        var blueColor = Math.floor(Math.random() * 255);
+
+        return "rgb(" + redColor + ", " + greenColor + ", " + blueColor + ")"
+    }
+
+    const squares = document.querySelectorAll(".square");
+    for (let i = 0; i < numeroCuadrados; i++) {
+        const resutaldoDelColor = asignarColoresTarjetas()
+        squares[i].style.backgroundColor = resutaldoDelColor;
+        colors.push(squares[i].style.backgroundColor);
+
+    }
+
+    colorsRandom = colors[Math.floor(Math.random() * numeroCuadrados)];
+    document.getElementById("aggColorRGB").innerHTML = colorsRandom;
+
+    function detectarClick(squareTal) {
+        squareTal.addEventListener("click", function () {
+            var colorRecibido = squareTal.style.backgroundColor
+
+            if (colorRecibido === colorsRandom) {
+                console.log("Si");
+                message.textContent = "Correcto";
+                changeColors(colorRecibido)
+
+            } else {
+                console.log("No");
+                this.style.background = "#232323"
+                message.textContent = "Inténtalo nuevamente";
+            }
+        })
+    }
+
+    function changeColors(color) {
+        for (var i = 0; i < squares.length; i++) {
+            squares[i].style.background = color
+            document.getElementById("cambiarAquiCuandoGano").style.backgroundColor = color;
+            document.getElementById("miBotonCheto").style.backgroundColor = color;
+
+        }
+    }
+
+    function recogerDiv() {
+        for (var i = 0; i < squares.length; i++) {
+            var squareTal = squares[i];
+            detectarClick(squareTal)
+        }
+    }
+
+    recogerDiv()
+
+}
+
+iniciarElJuegoMomentaneamente()
+
+function reiniciar() {
+    location.reload()
+}
+
+function easyGame() {
+    let rgb = "#232323";
+    numeroCuadrados = 3;
+    console.log(numeroCuadrados);
+    document.getElementById("del01").style.backgroundColor = rgb;
+    document.getElementById("del02").style.backgroundColor = rgb;
+    document.getElementById("del03").style.backgroundColor = rgb;
+
+    colorsRandom = colors[Math.floor(Math.random() * 3)];
+    document.getElementById("aggColorRGB").innerHTML = colorsRandom;
+    console.log(colorsRandom);
+
+}
+
+function hardGame() {
+
+    reiniciar()
+
+}
